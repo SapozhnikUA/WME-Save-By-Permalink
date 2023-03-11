e1682d54d3eecdb01b22320c81967d99e97d2549
SapozhnikUA <sapozhnik@gmail.com>
2023-03-11 13:43:59 +0200

    Anton

--- WME Update Segment By Permalink.user.js
+++ WME Update Segment By Permalink.user.js
@@ -12,14 +12,14 @@
 // https://waze.com/uk/editor?env=row&lat=46.59851&lon=33.07064&s=8379753821591&zoomLevel=18&segments=427057335#fwdMaxSpeed=50&lockRank=2&revMaxSpeed=30&flags_set=unpaved,headlights
 // #lockRank=2 (левел +1)
 
-// Переход по кординатам 
+// Переход по кординатам
 // W.map.setCenter(OpenLayers.Layer.SphericalMercator.forwardMercator(parseFloat(33.07064), parseFloat(46.59851)))
 
 (function main() {
   'use strict';
   let UpdateObject;
   let URL_LIST = [
-    'https://waze.com/uk/editor?env=row&lat=46.829602947&lon=32.74704568&s=3402715231638&zoomLevel=17&segments=381032854#lockRank=2',
+ 'https://waze.com/uk/editor?env=row&lat=46.829602947&lon=32.74704568&s=3402715231638&zoomLevel=17&segments=381032854#lockRank=2',
     'https://waze.com/uk/editor?env=row&lat=46.846613357&lon=32.75742926&s=3402715231638&zoomLevel=17&segments=206075007#lockRank=2',
     'https://waze.com/uk/editor?env=row&lat=46.844306247&lon=32.75657349&s=3402715231638&zoomLevel=17&segments=206075435#lockRank=2',
     'https://waze.com/uk/editor?env=row&lat=46.843228497&lon=32.75599945&s=3402715231638&zoomLevel=17&segments=206075439#lockRank=2',
@@ -273,8 +273,7 @@
     'https://waze.com/uk/editor?env=row&lat=45.379652927&lon=35.92709384&s=3402715231638&zoomLevel=17&segments=362181529#flags_unset=unpaved',
     'https://waze.com/uk/editor?env=row&lat=45.362755207&lon=35.97224245&s=3402715231638&zoomLevel=17&segments=405839867#flags_unset=unpaved',
     'https://waze.com/uk/editor?env=row&lat=45.349203377&lon=35.96882514&s=3402715231638&zoomLevel=17&segments=362182002#flags_unset=unpaved',
-    'https://waze.com/uk/editor?env=row&lat=45.363448347&lon=35.97993407&s=3402715231638&zoomLevel=17&segments=362558504#flags_unset=unpaved',
-  ];
+    'https://waze.com/uk/editor?env=row&lat=45.363448347&lon=35.97993407&s=3402715231638&zoomLevel=17&segments=362558504#flags_unset=unpaved',  ];
   const FLAGS_BIT = {
     tunnel: 0b00000001,
     // a     :  0b00000010,
@@ -288,7 +287,7 @@
 
   async function goThrowTheLinks() {
     var counter = 0;
-    var counter_save = 15000; // значение автосохранения
+    var counter_save = 50; // значение автосохранения
     if (URL_LIST.length < counter_save) counter_save = URL_LIST.length;
 
     for (const link of URL_LIST) {
@@ -300,18 +299,40 @@
         // Если есть хеш (#) проводим обновление
         if (url.hash) {
           await updateObjects(url);
-          console.log(counter + '---->' + counter_save);
-// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!          
-          if (++counter == counter_save) { // считаем автосохранения
-            //            await Promise.all(W.commands.request('save:start'),);
-            //            counter = 0
+          console.log(counter + '---->', counter_save, url);
+          // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+          if (++counter == counter_save || URL_LIST[URL_LIST.length -1] === link) { // считаем автосохранения
+
+     await save();
+            counter = 0;
           }
         }
       }
     }
   }
 
+  function save() {
+    //W.commands.request('save:start');
+
+    //W.editingMediator.get("saving")
+
+    return new Promise((resolve) => {
+      W.commands.request('save:start');
+      const interval = setInterval(() => {
+
+        if (!W.editingMediator.get("saving")) {
+          clearInterval(interval)
+          resolve()
+        }
+
+      }, 200)
+
+    });
+  }
+
+
   function moveMap(lon, lat) {
+    console.log('Сдвигаем...', lon, lat);
     return new Promise((resolve) => {
       W.map.setCenter(
         OpenLayers.Layer.SphericalMercator.forwardMercator(
@@ -319,30 +340,32 @@
           parseFloat(lat),
         ),
       );
-
-      document.addEventListener(
-        'wme-map-data-loaded',
-        () => {
-          setTimeout(() => resolve(), 2000)
-          //resolve()
-        },
-        {
-          once: true,
-        },
-      );
+      setTimeout(() => resolve(), 200)
     });
   }
+
+
+
+
   function waitLoadingData(id) {
     // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  Если нет ID (сегмент не читается или был удален)
     return new Promise((resolve) => {
-      const segment = W.model.segments.getObjectById(id);
-
-      if (!segment) {
-        console.log('Ожидаем загрузку...');
-        setTimeout(() => resolve(waitLoadingData(id)), 200);
-      } else {
-        resolve(segment);
-      }
+      console.log('Ожидаем загрузку...', id);
+      let counterWait = 0;
+      const interval = setInterval(() => {
+        const segment = W.model.segments.getObjectById(id);
+        if (segment) {
+          clearInterval(interval);
+          resolve(segment);
+        } else {
+          counterWait++;
+          if (counterWait > 50) {
+            clearInterval(interval);
+              console.log('Несуществующий сегмент:', id);
+            resolve(null);
+          }
+        }
+      },200);
     });
   }
 
@@ -350,50 +373,49 @@
   async function updateObjects(url) {
 
     const searchParams = new URLSearchParams(url.search.replace('?', '')); // Получаем данные в ссылке после "?"
-    const segments = await Promise.all(
+    const segments = (await Promise.all(
       searchParams.getAll('segments').map((id) => waitLoadingData(id)),
-    );
-    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    let flags = segments[0].attributes.flags//.toString(2); // как правильно получать flags ??????????????????????
-    // console.log(flags.toString(2));
+    )).filter(segment => segment);
+    if (segments.length) {
+      let flags = segments[0].attributes.flags//.toString(2); // как правильно получать flags ??????????????????????
+      // console.log(flags.toString(2));
 
 
-    const updateProps = {}; // создаем объект
-    const hashParams = new URLSearchParams(url.hash.replace('#', '')); // Создаем new экземпляр объекта
-    // получаем данные для загрузки
-    for (const [key, value] of hashParams.entries()) {
+      const updateProps = {}; // создаем объект
+      const hashParams = new URLSearchParams(url.hash.replace('#', '')); // Создаем new экземпляр объекта
+      // получаем данные для загрузки
+      for (const [key, value] of hashParams.entries()) {
 
-      // обрабатываем флаги
-      // снимаем ненужные
-      if (key == 'flags_unset') {
-        if (value.includes('unpaved')) flags &= ~(FLAGS_BIT.unpaved);
-        if (value.includes('tunnel')) flags &= ~(FLAGS_BIT.tunnel);
-        if (value.includes('headlights')) flags &= ~(FLAGS_BIT.headlights);
-        if (value.includes('nearbyHOV')) flags &= ~(FLAGS_BIT.nearbyHOV);
-        updateProps['flags'] = flags;
+        // обрабатываем флаги
+        // снимаем ненужные
+        if (key == 'flags_unset') {
+          if (value.includes('unpaved')) flags &= ~(FLAGS_BIT.unpaved);
+          if (value.includes('tunnel')) flags &= ~(FLAGS_BIT.tunnel);
+          if (value.includes('headlights')) flags &= ~(FLAGS_BIT.headlights);
+          if (value.includes('nearbyHOV')) flags &= ~(FLAGS_BIT.nearbyHOV);
+          updateProps['flags'] = flags;
+        }
+        // ставим нужные
+        if (key == 'flags_set') {
+          if (value.includes('unpaved')) flags |= (FLAGS_BIT.unpaved);
+          if (value.includes('tunnel')) flags |= (FLAGS_BIT.tunnel);
+          if (value.includes('headlights')) flags |= (FLAGS_BIT.headlights);
+          if (value.includes('nearbyHOV')) flags |= (FLAGS_BIT.nearbyHOV);
+          updateProps['flags'] = flags;
+        }
+
+        if (key != 'flags_set' && key != 'flags_unset') {
+          updateProps[key] = parseInt(value);
+        }
+        console.log('PL hash: ' + key + ' = ' + value);
       }
-      // ставим нужные
-      if (key == 'flags_set') {
-        if (value.includes('unpaved')) flags |= (FLAGS_BIT.unpaved);
-        if (value.includes('tunnel')) flags |= (FLAGS_BIT.tunnel);
-        if (value.includes('headlights')) flags |= (FLAGS_BIT.headlights);
-        if (value.includes('nearbyHOV')) flags |= (FLAGS_BIT.nearbyHOV);
-        updateProps['flags'] = flags;
-      }
+      //    console.log(updateProps);
 
-      if (key != 'flags_set' && key != 'flags_unset') {
-        updateProps[key] = value;
-      }
-      console.log('PL hash: ' + key + ' = ' + value);
+      await Promise.all(segments.map((segment) => {
+        W.model.actionManager.add(new UpdateObject(segment, updateProps));
+      }),);
+
     }
-    //    console.log(updateProps);
-
-    await Promise.all(segments.map((segment) => {
-      W.model.actionManager.add(new UpdateObject(segment, updateProps));
-    }),);
-
-
-    //W.commands.request('save:start');
   }
 
 
