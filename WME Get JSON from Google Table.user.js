855a0f1086da8fa9eda4a4976bd340c4b52e3e58
SapozhnikUA <sapozhnik@gmail.com>
2023-04-02 16:24:55 +0300

    new fix

--- WME Get JSON from Google Table.user.js
+++ WME Get JSON from Google Table.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        WME Get JSON from Google Table
 // @namespace   WazeUA
-// @version     0.0.1
+// @version     0.0.2
 // @description none
 // @author      Sapozhnik
 // @match       https://www.waze.com/uk/user/editor/sapozhnik
