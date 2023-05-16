9e4569a7b7443deec31d96bd5a0d228c03efffdb
SapozhnikUA <48523324+SapozhnikUA@users.noreply.github.com>
2023-05-16 20:08:02 +0300

    fix

--- WME Create POI from Google sheet.users.js
+++ WME Create POI from Google sheet.users.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        WME Create POI from Google sheet
 // @namespace   WazeUA
-// @version     0.0.26
+// @version     0.0.27
 // @description none
 // @author      Sapozhnik
 // @match       https://*.waze.com/editor*
@@ -55,7 +55,7 @@
         NewPoint.attributes.description = venue.description;
         NewPoint.attributes.aliases = venue.aliases;
         NewPoint.attributes.services = venue.services;
-        NewPoint.attributes.openingHours = push(venue.openingHours);
+        NewPoint.attributes.openingHours.push(venue.openingHours);
     
 
         // Клонируем ТФ
