8e7b12293f395645e14fb3fd16716219ad141e52
SapozhnikUA <48523324+SapozhnikUA@users.noreply.github.com>
2023-05-16 11:55:50 +0300

    start new version

--- WME Create POI from Google sheet.users.js
+++ WME Create POI from Google sheet.users.js
@@ -7,25 +7,33 @@
 // @match       https://*.waze.com/editor*
 // @match       https://*.waze.com/*/editor*
 // @require      https://greasyfork.org/scripts/452563-wme/code/WME.js
+// @require      https://github.com/SapozhnikUA/WME-Save-By-Permalink/raw/main/WME%20Get%20JSON%20from%20Google%20Table.user.js
 // @require      https://greasyfork.org/scripts/38421-wme-utils-navigationpoint/code/WME%20Utils%20-%20NavigationPoint.js
+// @connect      google.com
+// @connect      script.googleusercontent.com
+// @grant        GM_xmlhttpRequest
+// @grant        GM_addStyle
 // ==/UserScript==
 
+/* global GetJSON */
 /* global W */
 /* global require */
 /* global OpenLayers */
 /* global NavigationPoint */
 
 //https://www.waze.com/uk/editor?env=row&lat=50.43335&lon=30.43289&s=8644984831447&zoomLevel=18&venues=19923448.199431091.27863278
-  /**
-   * @link https://github.com/openlayers/openlayers
-   */
+/**
+ * @link https://github.com/openlayers/openlayers
+ */
 
-  (function main() {
+(function main() {
     'use strict';
+    const HASH = "AKfycbyqCEYHT1-jhQw8MZg1HCtKshro3bVJz7eGnG9rBl2BAZ1LmOxRKj-jOJ6EXJAZSPpMaw";
 
-    function createPoint (isResidential = false) {
+    function createPoint(data, isResidential = false) {
+        console.log('Data->', data);
 
-        let {lat, lon} ={ lat: 50.43590, lon: 30.43231 }
+        let { lat, lon } = { lat: 50.43590, lon: 30.43231 }
 
         let WazeFeatureVectorLandmark = require('Waze/Feature/Vector/Landmark')
         let WazeActionAddLandmark = require('Waze/Action/AddLandmark')
@@ -39,7 +47,7 @@
         let pointGeometry = new OpenLayers.Geometry.Point(lon, lat).transform('EPSG:4326', 'EPSG:900913') // !!!!!!!!!!!!!!!!!!!!!!!!!!1
 
         NewPoint.geometry = pointGeometry
-//        NewPoint.attributes.categories.push('OTHER') // CHARGING_STATION
+        //        NewPoint.attributes.categories.push('OTHER') // CHARGING_STATION
         NewPoint.attributes.categories.push('CHARGING_STATION') // CHARGING_STATION
         NewPoint.attributes.lockRank = lockRank
         NewPoint.attributes.residential = isResidential
@@ -47,74 +55,76 @@
         NewPoint.attributes.phone = '+380503832131';
 
         // Атрибуты charger station
-         const CHARGING_STATION = {};
-         const paymentMethods = ["APP"] ?? [];
+        const CHARGING_STATION = {};
+        const paymentMethods = ["APP"] ?? [];
         CHARGING_STATION["paymentMethods"] = ["APP", "MEMBERSHIP_CARD"] ?? [];
         CHARGING_STATION["network"] = "GO-TO U";
         CHARGING_STATION["costType"] = "FEE";
         CHARGING_STATION["chargingPorts"] = [
-                {
-                    "count": 1,
-                    "portId": "1",
-                    "connectorTypes": [
-                        "TYPE1"
-                    ],
-                    "maxChargeSpeedKw": 22
-                },
-                {
-                    "count": 1,
-                    "portId": "2",
-                    "connectorTypes": [
-                        "TYPE1"
-                    ],
-                    "maxChargeSpeedKw": 22
-                }
-            ];
-
-
-             if (Object.keys(CHARGING_STATION).length) {
-                NewPoint.attributes["categoryAttributes"] = {};
-                NewPoint.attributes.categoryAttributes["CHARGING_STATION"] = CHARGING_STATION;
+            {
+                "count": 1,
+                "portId": "1",
+                "connectorTypes": [
+                    "TYPE1"
+                ],
+                "maxChargeSpeedKw": 22
+            },
+            {
+                "count": 1,
+                "portId": "2",
+                "connectorTypes": [
+                    "TYPE1"
+                ],
+                "maxChargeSpeedKw": 22
             }
+        ];
+
+
+        if (Object.keys(CHARGING_STATION).length) {
+            NewPoint.attributes["categoryAttributes"] = {};
+            NewPoint.attributes.categoryAttributes["CHARGING_STATION"] = CHARGING_STATION;
+        }
 
 
 
 
-// Клонируем ТФ
-          NewPoint.attributes.entryExitPoints.push(new NavigationPoint(pointGeometry.clone()));
-       // Указываем адрес
-          NewPoint.attributes.name = "Название POI"
-          NewPoint.attributes.houseNumber = 555;
+        // Клонируем ТФ
+        NewPoint.attributes.entryExitPoints.push(new NavigationPoint(pointGeometry.clone()));
+        // Указываем адрес
+        NewPoint.attributes.name = "Название POI"
+        NewPoint.attributes.houseNumber = 555;
 
         let newAddressAttributes = {
-    "streetName": "вул. Михайла Донця",
-    "emptyStreet": false,
-    "cityName": "Київ",
-    "emptyCity": false,
-    "stateID": 1,
-    "countryID": 232,
-}
+            "streetName": "вул. Михайла Донця",
+            "emptyStreet": false,
+            "cityName": "Київ",
+            "emptyCity": false,
+            "stateID": 1,
+            "countryID": 232,
+        }
 
         //newAddressAttributes = {};
         console.log('NewPoint', NewPoint);
 
-// Создаем POI
+        // Создаем POI
         W.selectionManager.unselectAll()
         let addedLandmark = new WazeActionAddLandmark(NewPoint)
         W.model.actionManager.add(addedLandmark)
 
-// Добавляем адрес
+        // Добавляем адрес
         W.model.actionManager.add(new WazeActionUpdateFeatureAddress(NewPoint, newAddressAttributes))
         W.model.actionManager.add(new WazeActionUpdateObject(NewPoint, { houseNumber: NewPoint.attributes.houseNumber }))
         W.selectionManager.setSelectedModels([addedLandmark.venue])
         console.log('The point was created.')
-      }
+    }
 
 
     function bootstrap() {
         if (W && W.loginManager && W.loginManager.user && W.map && require) {
-//            UpdateObject = require('Waze/Action/UpdateObject');
-            createPoint();
+            const getJSON = new GetJSON(HASH);
+            getJSON.getJsonData().then(data => {
+                createPoint(data);
+            });
         } else {
             setTimeout(bootstrap, 5000);
         }
