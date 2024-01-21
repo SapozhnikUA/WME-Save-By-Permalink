b240632853de2f3df8f5b3ac9a71cb772d77e5b2
SapozhnikUA <48523324+SapozhnikUA@users.noreply.github.com>
2024-01-21 20:02:28 +0200

    update

--- WME Create POI from Google sheet.users.js
+++ WME Create POI from Google sheet.users.js
@@ -29,14 +29,17 @@
     const HASH = "AKfycbyqCEYHT1-jhQw8MZg1HCtKshro3bVJz7eGnG9rBl2BAZ1LmOxRKj-jOJ6EXJAZSPpMaw";
 
     function createPoint(venue, isResidential = false) {
-//        console.log('Data->', venue);
-        if (venue.categories == "RPP"){
+        //console.clear();
+        console.group('Add POI:');
+        console.log('Venue:', venue);
+        console.log('Распаковка:', venue.streetName, venue.houseNumber, String(venue.categories), String(venue.cityName));
+
+        if (venue.categories == "RPP") {
             isResidential = true;
         }
 
         let { lat, lon } = { lat: Number(venue.lat), lon: Number(venue.lon) }
-//        console.log ("lat",lat);
-        if (isResidential){
+        if (isResidential) {
             lat += 0.00002;
             lon += 0.00002;
         }
@@ -49,24 +52,24 @@
         let address = {};
         let lockRank = 1;
         let pointGeometry = new OpenLayers.Geometry.Point(lon, lat).transform('EPSG:4326', 'EPSG:900913') // !!!!!!!!!!!!!!!!!!!!!!!!!!1
-            let NewPoint = new WazeFeatureVectorLandmark({
-      geoJSONGeometry: W.userscripts.toGeoJSONGeometry(pointGeometry)
-    })
+        let NewPoint = new WazeFeatureVectorLandmark({
+            geoJSONGeometry: W.userscripts.toGeoJSONGeometry(pointGeometry)
+        })
 
         NewPoint.geometry = pointGeometry;
         NewPoint.attributes.categories.push(venue.categories); //
         NewPoint.attributes.lockRank = venue.lockRank;
         NewPoint.attributes.residential = isResidential;
-        if (!isResidential){
-            NewPoint.attributes.url = venue.url;
-            NewPoint.attributes.phone = venue.phone;
-            NewPoint.attributes.description = venue.description;
-            NewPoint.attributes.aliases = venue.aliases;
-            NewPoint.attributes.services = venue.services;
-            NewPoint.attributes.openingHours = venue.openingHours.map(item => new OpeningHour(item))
+        if (!isResidential) {
+            //            NewPoint.attributes.url = venue.url;
+            //            NewPoint.attributes.phone = venue.phone;
+            //            NewPoint.attributes.description = venue.description;
+            //            NewPoint.attributes.aliases = venue.aliases;
+            //            NewPoint.attributes.services = venue.services;
+            //            NewPoint.attributes.openingHours = venue.openingHours.map(item => new OpeningHour(item))
         }
         // Клонируем ТФ
-          NewPoint.attributes.entryExitPoints.push(new entryPoint({primary: true, point: W.userscripts.toGeoJSONGeometry(pointGeometry.clone())}));
+        NewPoint.attributes.entryExitPoints.push(new entryPoint({ primary: true, point: W.userscripts.toGeoJSONGeometry(pointGeometry.clone()) }));
 
 
         // Указываем адрес
@@ -82,7 +85,7 @@
             "countryID": 232,
         }
 
-//        console.log('NewPoint', NewPoint.attributes);
+        //        console.log('NewPoint', NewPoint.attributes);
 
         // Создаем POI
         W.selectionManager.unselectAll()
@@ -105,22 +108,30 @@
         // document.querySelector('.add-opening-hour .waze-btn-blue').click()
 
         console.log('The point was created.')
+        console.groupEnd();
     }
 
 
-    function bootstrap() {
+    async function bootstrap() {
+        const getJSON = new GetJSON(HASH);
         if (W && W.loginManager && W.loginManager.user && W.map && require) {
-            const getJSON = new GetJSON(HASH);
-            getJSON.getJsonData().then(data => {
-                for (let venue of data.venues) {
-                    createPoint(venue);
+            getJSON.getJsonData().then(async data => {
+                if (data.dataStatus == 'success') {
+                    console.log('Status', data.dataStatus)
+                    for (let venue of data.venues) {
+                        let temp = data.venues[0].categorіes;
+                        console.log('data-venue.categorіes.', temp);
+                        //                    venue.categories = "ddddd";
+                        await createPoint(venue);
+                    }
                 }
             });
         } else {
-            setTimeout(bootstrap, 2500);
+            setTimeout(bootstrap, 5000);
         }
     }
 
     bootstrap();
+
 })();
 
